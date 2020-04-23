ddd57142d4ebed529704dc3df0256ba55f6c702f
Daniel Morrison <daniemo2@cisco.com>
2020-04-23 20:18:43 +0100

    Added other works

--- gulpfile.js
+++ gulpfile.js
@@ -5,55 +5,58 @@ var gulp = require('gulp'),
     sourcemaps = require('gulp-sourcemaps'),
     browserSync = require('browser-sync').create(),
     browserSyncReuseTab = require('browser-sync-reuse-tab')(browserSync),
-    zip = require('gulp-zip'),
-    ghPages = require('gulp-gh-pages');
+    zip = require('gulp-zip');
 
 
-gulp.task('sass', function() {
+gulp.task('sass', function () {
     return gulp.src('app/css/**/*.scss')
         .pipe(sourcemaps.init())
         .pipe(concat('main.css'))
-        .pipe(sass({errLogToConsole: true}))
+        .pipe(sass({ errLogToConsole: true }))
         .pipe(cssnano())
         .pipe(sourcemaps.write())
         .pipe(gulp.dest('app/css'));
 });
 
-gulp.task('browserSync', function() {
+gulp.task('browserSync', function (done) {
     browserSync.init({
         server: {
             baseDir: 'app'
         },
         notify: false,
     }, browserSyncReuseTab);
+
+    done();
 });
 
-gulp.task('html:dist', function() {
+// BrowserSync Reload
+function browserSyncReload(done) {
+    browserSync.reload();
+    done();
+}
+
+gulp.task('html:dist', function () {
     return gulp.src(['app/index.html'])
         .pipe(gulp.dest('dist'));
 });
-gulp.task('css:dist', function() {
+
+gulp.task('css:dist', function () {
     return gulp.src(['app/css/main.css', 'app/css/normalize.css'])
         .pipe(gulp.dest('dist/css'));
 });
-gulp.task('img:dist', function() {
+
+gulp.task('img:dist', function () {
     return gulp.src(['app/images/*'])
         .pipe(gulp.dest('dist/images'));
 });
-gulp.task('dist', ['html:dist', 'css:dist', 'img:dist'], function() {
-    gulp.src('dist/**/')
+
+gulp.task('dist', gulp.parallel('html:dist', 'css:dist', 'img:dist', function () {
+    return gulp.src('dist/**/')
         .pipe(zip('archive.zip'))
         .pipe(gulp.dest('dist'));
-});
+}));
 
-gulp.task('default', ['browserSync', 'sass'], function() {
-    var reload = browserSync.reload;
-
-    gulp.watch('app/index.html', reload);
-    gulp.watch('app/css/**/*.scss', ['sass', reload]);
-});
-
-gulp.task('deploy', function() {
-    gulp.src('./dist/**/*')
-        .pipe(ghPages());
-});
+gulp.task('default', gulp.parallel('browserSync', 'sass', function () {
+    gulp.watch('app/index.html', browserSyncReload);
+    gulp.watch('app/css/**/*.scss', gulp.series('sass', browserSyncReload));
+}));
